8b74e29474d1b0b29e3d0ea24273eee9ef0fcb05
hpcat1010 <132513839+hpcat1010@users.noreply.github.com>
2024-03-11 18:51:45 -0300

    Add files via upload
    no se

--- haskel/tp1 haskel/Stack.hs
+++ haskel/tp1 haskel/Stack.hs
@@ -7,8 +7,21 @@ import Route
 data Stack = Sta [ Container ] Int deriving (Eq, Show)
 
 newS :: Int -> Stack                          -- construye una Pila con la capacidad indicada 
+newS n = Sta [] n
+--newS n = [1..n]
+
 freeCellsS :: Stack -> Int                    -- responde la celdas disponibles en la pila
+freeCellsS (Sta _ n) = n
+
 stackS :: Stack -> Container -> Stack         -- apila el contenedor indicado en la pila
+
 netS :: Stack -> Int                          -- responde el peso neto de los contenedores en la pila
+netS (Sta _ pesos) =  sum [pesos]
+
+netC :: Container -> Int             -- responde el peso en toneladas del contenedor
+netC (Con ciudad_x peso_x) = peso_x
+
+
 holdsS :: Stack -> Container -> Route -> Bool -- indica si la pila puede aceptar el contenedor considerando las ciudades en la ruta
+
 popS :: Stack -> String -> Stack              -- quita del tope los contenedores con destino en la ciudad indicada
