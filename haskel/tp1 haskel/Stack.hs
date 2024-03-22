6b3c3af9fefbfbc6726109f3e19ec95e28f98723
Gaia Tornielli <gaiatornielli@Gaias-MacBook-Air.local>
2024-03-22 09:48:02 -0300

    s

--- haskel/tp1 haskel/Stack.hs
+++ haskel/tp1 haskel/Stack.hs
@@ -1,4 +1,4 @@
-module Stack ( Stack, newS, freeCellsS, stackS, netS, holdsS, popS )
+module Stack -- ( Stack, newS, freeCellsS, stackS, netS, holdsS, popS )
  where
 
 import Container
@@ -13,15 +13,15 @@ newS n = Sta [] n
 freeCellsS :: Stack -> Int                    -- responde la celdas disponibles en la pila
 freeCellsS (Sta _ n) = n
 
-stackS :: Stack -> Container -> Stack         -- apila el contenedor indicado en la pila
+-- stackS :: Stack -> Container -> Stack         -- apila el contenedor indicado en la pila
 
 netS :: Stack -> Int                          -- responde el peso neto de los contenedores en la pila
 netS (Sta _ pesos) =  sum [pesos]
 
-netC :: Container -> Int             -- responde el peso en toneladas del contenedor
-netC (Con ciudad_x peso_x) = peso_x
--- hay que buscar una función dentro de container que devuelva el peso peso_x 
+-- netC :: Container -> Int             -- responde el peso en toneladas del contenedor
+-- netC (Con ciudad_x peso_x) = peso_x
+---- hay que buscar una función dentro de container que devuelva el peso peso_x 
 
-holdsS :: Stack -> Container -> Route -> Bool -- indica si la pila puede aceptar el contenedor considerando las ciudades en la ruta
+-- holdsS :: Stack -> Container -> Route -> Bool -- indica si la pila puede aceptar el contenedor considerando las ciudades en la ruta
 
-popS :: Stack -> String -> Stack              -- quita del tope los contenedores con destino en la ciudad indicada
+-- popS :: Stack -> String -> Stack              -- quita del tope los contenedores con destino en la ciudad indicada
