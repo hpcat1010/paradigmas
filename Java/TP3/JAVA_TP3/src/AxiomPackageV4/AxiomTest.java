7af8e90ab273f452fb202fadafbcb9e8a83ea4b4
hpcat1010 <132513839+hpcat1010@users.noreply.github.com>
2024-05-24 13:27:04 -0300

    mejorando test

--- Java/TP3/JAVA_TP3/src/AxiomPackageV4/AxiomTest.java
+++ Java/TP3/JAVA_TP3/src/AxiomPackageV4/AxiomTest.java
@@ -24,105 +24,116 @@ public class AxiomTest {
     @Test
     public void testIncreaseSpeed() {
         Axiom axiom = new Axiom();
-        axiom.Message("i");
+        axiom.ProcessMessage("i");
         assertEquals(1, axiom.currentSpeed());
     }
 
     @Test
     public void testDecreaseSpeed() {
         Axiom axiom = new Axiom();
-        axiom.Message("s");
+        axiom.ProcessMessage("s");
         assertEquals(0, axiom.currentSpeed());
     }
 
     @Test
     public void testIncreaseSpeedTwice() {
         Axiom axiom = new Axiom();
-        axiom.Message("i");
-        axiom.Message("i");
+        axiom.ProcessMessage("i");
+        axiom.ProcessMessage("i");
         assertEquals(2, axiom.currentSpeed());
     }
 
     @Test
     public void testDecreaseSpeedTwice() {
         Axiom axiom = new Axiom();
-        axiom.Message("s");
-        axiom.Message("s");
+        axiom.ProcessMessage("s");
+        axiom.ProcessMessage("s");
         assertEquals(0, axiom.currentSpeed());
     }
 
     @Test
     public void testTurnRight() {
         Axiom axiom = new Axiom();
-        axiom.Message("r");
+        axiom.ProcessMessage("r");
         assertEquals("East", axiom.currentDirection());
     }
 
     @Test
     public void testTurnLeft() {
         Axiom axiom = new Axiom();
-        axiom.Message("l");
+        axiom.ProcessMessage("l");
         assertEquals("West", axiom.currentDirection());
     }
 
     @Test
     public void testTurnRightThenLeft() {
         Axiom axiom = new Axiom();
-        axiom.Message("r");
-        axiom.Message("l");
+        axiom.ProcessMessage("r");
+        axiom.ProcessMessage("l");
         assertEquals("North", axiom.currentDirection());
     }
 
     @Test
     public void deplySonda() {
         Axiom axiom = new Axiom();
-        axiom.Message("i");
-        axiom.Message("d");
+        axiom.ProcessMessage("i");
+        axiom.ProcessMessage("d");
         assertTrue(axiom.currentSonda());
     }
 
     @Test
     public void retractSonda() {
         Axiom axiom = new Axiom();
-        axiom.Message("i");
-        axiom.Message("d");
-        axiom.Message("f");
+        axiom.ProcessMessage("i");
+        axiom.ProcessMessage("d");
+        axiom.ProcessMessage("f");
         assertFalse(axiom.currentSonda());
     }
 
     @Test
     public void testSondaCannotStop() {
         Axiom axiom = new Axiom();
-        axiom.Message("i");
-        axiom.Message("d");
-        assertThrowsLike(() -> axiom.Message("s"));
+        axiom.ProcessMessage("i");
+        axiom.ProcessMessage("d");
+        assertThrowsLike(() -> axiom.ProcessMessage("s"));
+    }
+
+    @Test
+    public void testSondaCanSlowDown() {
+        Axiom axiom = new Axiom();
+        axiom.ProcessMessage("i");
+        axiom.ProcessMessage("i");
+        axiom.ProcessMessage("d");
+        axiom.ProcessMessage("s");
+        assertEquals(1,axiom.currentSpeed());
+
     }
 
     @Test
     public void testSondaCannotTurnRight() {
         Axiom axiom = new Axiom();
-        axiom.Message("i");
-        axiom.Message("d");
-        assertThrowsLike(() -> axiom.Message("r"));
+        axiom.ProcessMessage("i");
+        axiom.ProcessMessage("d");
+        assertThrowsLike(() -> axiom.ProcessMessage("r"));
     }
 
     @Test
     public void testSondaCannotTurnLeft() {
         Axiom axiom = new Axiom();
-        axiom.Message("i");
-        axiom.Message("d");
-        assertThrowsLike(() -> axiom.Message("l"));
+        axiom.ProcessMessage("i");
+        axiom.ProcessMessage("d");
+        assertThrowsLike(() -> axiom.ProcessMessage("l"));
     }
 
     @Test
     public void testSondaCannotDeployWhenStopped() {
         Axiom axiom = new Axiom();
-        assertThrowsLike(() -> axiom.Message("d"));
+        assertThrowsLike(() -> axiom.ProcessMessage("d"));
     }
     @Test
     public void testDeployAndRetractSonda() {
         Axiom axiom = new Axiom();
-        axiom.Message("idfi");
+        axiom.ProcessMessage("idfi");
         assertEquals(2, axiom.currentSpeed());
         assertFalse(axiom.currentSonda());
     }
@@ -130,7 +141,7 @@ public class AxiomTest {
     @Test
     public void testTwoCommands() {
         Axiom axiom = new Axiom();
-        axiom.Message("ii");
+        axiom.ProcessMessage("ii");
         assertEquals(2, axiom.currentSpeed());
 
     }
@@ -138,7 +149,7 @@ public class AxiomTest {
     @Test
     public void testMultipleCommands() {
         Axiom axiom = new Axiom();
-        axiom.Message("iirlrrllll");
+        axiom.ProcessMessage("iirlrrllll");
         assertEquals(2, axiom.currentSpeed());
         assertEquals("South", axiom.currentDirection());
         assertFalse(axiom.currentSonda());
@@ -147,13 +158,13 @@ public class AxiomTest {
     @Test
     public void testInvalidCommand() {
         Axiom axiom = new Axiom();
-        assertThrows(RuntimeException.class, () -> axiom.Message("x"));
+        assertThrows(RuntimeException.class, () -> axiom.ProcessMessage("x"));
     }
 
     @Test
     public void testEmptyCommand() {
         Axiom axiom = new Axiom();
-        axiom.Message("");
+        axiom.ProcessMessage("");
         assertEquals(0, axiom.currentSpeed());
         assertEquals("North", axiom.currentDirection());
         assertFalse(axiom.currentSonda());
@@ -161,7 +172,7 @@ public class AxiomTest {
     @Test
     public void testAllCommands() {
         Axiom axiom = new Axiom();
-        axiom.Message("iirlrrllliiiddididfrld");
+        axiom.ProcessMessage("iirlrrllliiiddididfrld");
         assertEquals(7, axiom.currentSpeed());
         assertEquals("West", axiom.currentDirection());
         assertTrue(axiom.currentSonda());
